cd774e3c2bfa27b167f6966c467ffaef927974be
Amish Bhatnagar <amishbroemailcom@gmail.com>
2025-04-09 01:51:18 +0530

    Added remainng staged files

---
+++ src/utils/logger.js
@@ -0,0 +1,32 @@
+const winston = require ("winston")
+
+const logger = winston.createLogger({
+    level : process.env.NODE_ENV === "production" ? "info" : "debug", // If it is in production it will log only error , warning and when it is in dev it will log debug
+    format : winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.splat(),
+        winston.format.errors({ stack : ture }),
+        winston.format.json()
+    ),
+    defaultMeta : {service : "identity-service"},
+    transports : [
+        new winston.transports.Console({
+            format : winston.format.combine(
+                winston.format.colorize(),     // Adds color to logs based on level 
+                winston.format.simple()     // Just message and level
+            ),
+
+        }),
+        new winston.transports.File({
+            filename : "error.log",
+            level : "error"
+        }),
+
+        new winston.transports.File({
+            filename : "combined.log"
+        })
+    ]
+        
+})
+
+module.exports = logger
